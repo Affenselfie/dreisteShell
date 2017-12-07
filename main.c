8cfdf0f901ae9a1b067f8bc17812f4e03362b106
Affenselfie <maximilian.kindt@mailbox.tu-dresden.de>
2017-12-07 16:25:29 +0100

    close #1

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+#define MAX_LENGTH  1024
+
+int main(int argc, char *argv) {
+    char line[MAX_LENGTH];
+
+    FILE *f =fopen("./log", "a");
+
+    while (1) {
+
+
+        printf("$ ");
+        if (!fgets(line, MAX_LENGTH, stdin)) break;
+
+        int result = fputs(line, f);
+
+        if (result < 0) return 1;
+
+
+
+    }
+
+    fclose(f);
+
+    return 0;
+}
