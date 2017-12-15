60a7a4f12bb64eba55a4af53ebe31dbe31c13925
Maximilian Kindt <maximilian.kindt@mailbox.tu-dresden.de>
2017-12-15 20:27:43 +0100

    #9 Close #10

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <signal.h>
 
 #define MAX_LENGTH  512
 
@@ -9,26 +11,36 @@ int main(int argc, char *argv) {
 
     FILE *f = NULL;
 
+
+
     int result = 0;
-    for(int i = 0;i<256;i++) {
+    for (int i = 0; i < 256; i++) {
+
 
         f = fopen("./log", "a");
         if (f == NULL) return 1;
 
-	if(i == 0) {
-	    time_t t;
-	    time(&t);
-	    result = fputs("LOG_START\n", f);
-   	    if (result < 0) return 1;
-	    result = fputs(ctime(&t), f);
-   	    if (result < 0) return 1;
-	}
+        if (i == 0) {
+            time_t *t = malloc(sizeof(time_t));
+            time(t);
+            result = fputs("LOG_START\n", f);
+            if (result < 0) return 1;
+            result = fputs(ctime(t), f);
+            free(t);
+            t = NULL;
+            if (result < 0) return 1;
+
+        }
 
         printf("$ ");
         if (!fgets(line, MAX_LENGTH, stdin)) break;
 
+
         result = fputs(line, f);
         if (result < 0) return 1;
+
+        int comp_result = strcmp(line, "exit\n");
+        if (comp_result == 0) return 0;
 
         fclose(f);
     }
