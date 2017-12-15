bfe4202ceb08ebfe6b8d166b57d7b5ab0e611b90
Sönke <me@eknoes.de>
2017-12-15 20:35:05 +0100

    Restructuring, is now able to react on CTRL+C

--- main.c
+++ main.c
@@ -1,38 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <signal.h>
+#include <string.h>
 
-#define MAX_LENGTH  512
+#define MAX_LENGTH 512
+
+void SIGINTHandler(int);
+
+FILE *f = NULL;
+int result = 0;
 
 int main(int argc, char *argv) {
     char line[MAX_LENGTH];
 
-    FILE *f = NULL;
+    //Register SIGINTHANDLER
+     signal(SIGINT, SIGINTHandler);
 
-    int result = 0;
-    for(int i = 0;i<256;i++) {
+    // Open File and write Log Start
+    f = fopen("./log", "a");
+    if (f == NULL) return 1;
 
-        f = fopen("./log", "a");
-        if (f == NULL) return 1;
+    time_t *t = malloc(sizeof(time_t));
+    time(t);
+    result = fputs("LOG_START\n", f);
+    if (result < 0) return 1;
+    result = fputs(ctime(t), f);
+    free(t);
+    t = NULL;
+    if (result < 0) return 1;
 
-	if(i == 0) {
-	    time_t t;
-	    time(&t);
-	    result = fputs("LOG_START\n", f);
-   	    if (result < 0) return 1;
-	    result = fputs(ctime(&t), f);
-   	    if (result < 0) return 1;
-	}
 
-        printf("$ ");
-        if (!fgets(line, MAX_LENGTH, stdin)) break;
-
+    printf("$ ");
+    // "Fake" Terminal and log input
+    while(fgets(line, MAX_LENGTH, stdin) != NULL) {
         result = fputs(line, f);
         if (result < 0) return 1;
 
-        fclose(f);
+        int comp_result = strcmp(line, "exit\n");
+        if (comp_result == 0) return 0;
+
+        printf("$ ");
     }
 
+    time(t);
+    result = fputs("LOG_END at ", f);
+      if (result < 0) return 1;
+    result = fputs(ctime(t), f);
+      if (result < 0) return 1;
     fclose(f);
     return 0;
+}
+
+void SIGINTHandler(int signal) {
+  time_t *t = malloc(sizeof(time_t));
+  time(t);
+  result = fputs("LOG_END at ", f);
+    if (result < 0) exit(1);
+  result = fputs(ctime(t), f);
+    if (result < 0) exit(1);
+  fclose(f);
+  exit(0);
 }
